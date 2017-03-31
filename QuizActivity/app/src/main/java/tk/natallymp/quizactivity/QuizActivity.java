8fa9265e2ba70cf4b6188237eb167a470854250c
x2wing <x2wing@gmail.com>
2017-03-31 16:49:59 +0500

    QuizActivity updated

--- QuizActivity/app/src/main/java/tk/natallymp/quizactivity/QuizActivity.java
+++ QuizActivity/app/src/main/java/tk/natallymp/quizactivity/QuizActivity.java
@@ -2,6 +2,7 @@ package tk.natallymp.quizactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +15,9 @@ public class QuizActivity extends AppCompatActivity {
     private Button mNextbutton;
     private Button mPreviousbutton;
     private TextView mQuestionTextView;
+
+    private static final String TAG="QuizActivity";
+    private static final String KEY_INDEX="index";
 
     private Question[] mQuestionBank = new Question[] {
             new Question(R.string.question_oceans, true),
@@ -29,6 +33,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called in QuizActivity");
         setContentView(R.layout.activity_quiz);
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +42,12 @@ public class QuizActivity extends AppCompatActivity {
                 nextQuestion();
             }
         });
-
+        // получаем значение mCurrentIndex предыдущего экземпляра QuizActivity,
+        // уничтоженного при повороте телефона
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+        //показать новый вопрос
         updateQuestion();
 
 
@@ -76,6 +86,39 @@ public class QuizActivity extends AppCompatActivity {
                 previousQuestion();
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void previousQuestion(){
